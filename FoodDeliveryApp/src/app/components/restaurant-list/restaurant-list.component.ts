5c6c7857556b0b3b5e6b861be727ae4ebbbcd3f0
chandini287 <chanadapa290@gmail.com>
2025-04-05 11:24:13 +0530

    Update restaurant-list.component.ts

--- FoodDeliveryApp/src/app/components/restaurant-list/restaurant-list.component.ts
+++ FoodDeliveryApp/src/app/components/restaurant-list/restaurant-list.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { RestaurantService } from '../../services/restaurant.service';
 import { Restaurant } from '../../models/restaurant.model';
- 
+
 @Component({
   selector: 'app-restaurant-list',
   standalone: true,
